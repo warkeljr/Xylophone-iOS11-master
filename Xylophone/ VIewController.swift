af10dd556b3060544c9096f9d9eca85d63c7b5ca
Wim van Arkel <warkel@me.com>
2017-11-20 09:39:39 +0100

    Fixed the global variable soundfile

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,24 +12,20 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate{
     
     var audioPlayer: AVAudioPlayer!
-    var selectedSoundFileName : String = ""
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func notePressed(_ sender: UIButton) {
         
-        selectedSoundFileName = soundArray[sender.tag - 1]
-        print(selectedSoundFileName)
-        
-        playSound()
+        playSound(SoundFileName: soundArray[sender.tag - 1])
         
     }
     
-    func playSound(){
-        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+    func playSound(SoundFileName: String){
+        let soundURL = Bundle.main.url(forResource: SoundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
